465e553cc73e525e947e5cbde07aa775474a10fc
JW-Ellis <jwilliamellis@gmail.com>
2021-06-12 14:27:38 -0500

    Move index route out of main index

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 8000;
-
 const app = express();
+const cors = require('cors');
 
-app.get('/setlist', (req, res) => {
-  res.json({ message: '7/11 was a part time job' });
-});
+// ROUTES
+const index = require('./routes/index.js');
+const login = require('./routes/login.js');
+
+const PORT = process.env.PORT || 8000;
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/', index);
+app.use('/login', login);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);

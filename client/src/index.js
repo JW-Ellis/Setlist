ec37616ef3bf9c997cc9bb0797f6cfc469495121
Justin Ellis <jwilliamellis@gmail.com>
2022-05-10 21:58:48 -0700

    Update client component paths

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import App from "./App";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>

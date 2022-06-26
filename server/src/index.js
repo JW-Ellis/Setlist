30a41663e1d6d6da43a90148ab1ddb73fc056d2c
Justin Ellis <jwilliamellis@gmail.com>
2022-06-26 10:21:20 -0700

    Add cors configs

--- server/src/index.js
+++ server/src/index.js
@@ -8,14 +8,20 @@ const app = express();
 const index = require("./routes/index.js");
 const login = require("./routes/login.js");
 const logged = require("./routes/logged.js");
+const playlist = require("./routes/playlist.js");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/", index);
 app.use("/login", login);
 app.use("/logged", logged);
+app.use("/playlist", playlist);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
